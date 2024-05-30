5976d9a99cce65a2ce4744ce7216b3e56ff89ccf
AndyLevinson <151685465+AndyLevinson@users.noreply.github.com>
2024-05-30 09:04:25 -0300

    Button activates colorchange

--- Assets/FoquitoScript.cs
+++ Assets/FoquitoScript.cs
@@ -50,6 +50,8 @@ public class FoquitoScript : MonoBehaviour
 
     public void ActivateRepeating(float t)
     {
-        InvokeRepeating(nameof(ActivateNextLight),0,t);
+        CancelInvoke();
+        InvokeRepeating(nameof(ActivateNextLight), 0, t);
+
     }
 }
